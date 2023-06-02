3f9be726148cde6d47b23e7b9cce86158ff624b7
Arminonly <amiaby2016@gmail.com>
2023-06-02 23:06:04 +0300

    'env_local'

--- src/pages/Auth/Components/IsMan.js
+++ src/pages/Auth/Components/IsMan.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import {Form, Radio } from 'antd';
+import React from 'react';
+import { Form, Radio } from 'antd';
 
-
-const IsMan = ({value,onChange}) => {
-  console.log(value);
-  
-   
+const IsMan = ({ value, onChange }) => {
   return (
     <>
-          <Form.Item
-          name="isMan"
-          label="isMan"
-          rules={[
-            {
-              required: true
-            }
-          ]}
-        >
-          <Radio.Group onChange={onChange} value={value}>
-            <Radio value={false}>male</Radio>
-            <Radio value={true}>female</Radio>
-          </Radio.Group>
-        </Form.Item> 
+      <Form.Item
+        name="isMan"
+        label="isMan"
+        rules={[
+          {
+            required: true
+          }
+        ]}
+      >
+        <Radio.Group onChange={onChange} value={value}>
+          <Radio value={false}>male</Radio>
+          <Radio value={true}>female</Radio>
+        </Radio.Group>
+      </Form.Item>
     </>
-  )
-}
+  );
+};
 
-export default IsMan
+export default IsMan;
